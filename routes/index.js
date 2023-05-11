9fbc849bca5d50d2170da33f56709fd30da1923c
wallylime <82895781+wallylime@users.noreply.github.com>
2023-05-11 14:16:44 -0600

    added swagger stuff for api docs

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,7 @@
 const express = require('express');
 const router = express.Router();
 
-//Any file paths with /contacts will use the contacts.js file in routes
 router.use('/contacts', require('./contacts'));
+router.use('/', require('./swagger'));
 
-//Making this available to other files that need to use it
 module.exports = router;
---
+++ swagger.js
@@ -0,0 +1,20 @@
+const swaggerAutogen = require('swagger-autogen')();
+const doc = {
+  info: {
+    title: 'My API',
+    description: 'Contacts API',
+  },
+  host: 'localhost:8080',
+  schemes: ['http'],
+};
+
+const outputFile = './swagger.json';
+const endpointsFiles = ['./routes/index.js'];
+
+// generate swagger.json
+swaggerAutogen(outputFile, endpointsFiles, doc);
+
+// Run server after it gets generated
+// swaggerAutogen(outputFile, endpointsFiles, doc).then(async () => {
+//   await import('./index.js');
+// });
